e62b9c112da30a80224f8f8fb0ca6a22012c2f3a
yao <myspiritcrazy@gmail.com>
2016-01-03 15:13:24 -0800

    use opencv library to show image

--- ImageComparison.py
+++ ImageComparison.py
@@ -3,5 +3,12 @@ import cv2
 
 # Load an color image in grayscale
 img = cv2.imread('baybridge.jpg',0)
+cv2.imshow('image',img)
+k = cv2.waitKey(0)
+if k == 27:         # wait for ESC key to exit
+    cv2.destroyAllWindows()
+elif k == ord('s'): # wait for 's' key to save and exit
+    cv2.imwrite('messigray.png',img)
+    cv2.destroyAllWindows()
 
 # print "Hello World"
