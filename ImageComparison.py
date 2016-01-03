d0ef3199543b362432e5134535a471fb53bdd33a
yao <myspiritcrazy@gmail.com>
2016-01-03 15:16:18 -0800

    use native image library to read image

--- ImageComparison.py
+++ ImageComparison.py
@@ -1,14 +1,7 @@
-import numpy as np
-import cv2
+import os,sys
+import Image
+jpgfile = Image.open("baybridge.jpg")
 
-# Load an color image in grayscale
-img = cv2.imread('baybridge.jpg',0)
-cv2.imshow('image',img)
-k = cv2.waitKey(0)
-if k == 27:         # wait for ESC key to exit
-    cv2.destroyAllWindows()
-elif k == ord('s'): # wait for 's' key to save and exit
-    cv2.imwrite('messigray.png',img)
-    cv2.destroyAllWindows()
+print jpgfile.bits, jpgfile.size, jpgfile.format
 
 # print "Hello World"
